be82d39f50bbd6df195aae1c4d4cf57a44ff2a24
bianle <229371515@qq.com>
2015-09-28 13:27:10 +0800

    Site updated: 2015-09-28 14:26:41

---
+++ static/monitor_resp_time_user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        监控-响应时间
+// @author      边乐
+// @description 监控-响应时间
+// @namespace   http://
+// @updateURL   https://userscripts.org/scripts/source/175746.meta.js
+// @downloadURL https://userscripts.org/scripts/source/175746.user.js
+// @icon        http://img.duoluohua.com/appimg/script_baidumusicscript_icon_48.png
+// @license     GPL version 3
+// @encoding    utf-8
+// @date        12/08/2013
+// @modified    06/12/2014
+// @include     http://music.baidu.com/song/*
+// @include     http://y.baidu.com/*
+// @resource loadingimg_1 http://tieba.baidu.com/tb/img/loading.gif
+// @resource loadingimg_2 http://tieba.baidu.com/tb/static-ihome/img/loading2.gif
+// @resource downimg http://bcs.duapp.com/opensource/images/appimg/min_red_download.png
+// @grant       GM_xmlhttpRequest
+// @grant       GM_getResourceURL
+// @run-at      document-end
+// @version     1.3.2
+// ==/UserScript==
