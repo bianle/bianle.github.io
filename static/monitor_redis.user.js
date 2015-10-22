bafd2a8c3c71be17d073ac4efed73b5d640829f2
bianle <229371515@qq.com>
2015-10-22 14:32:42 +0800

    Site updated: 2015-10-22 14:32:29

--- static/monitor_redis.user.js
+++ static/monitor_redis.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name        ����-Redis
-// @author      ����
-// @description ����-Redis
+// @name        监控-Redis
+// @author      边乐
+// @description 监控-Redis
 // @namespace   http://2le.me
 // @updateURL   http://2le.me/static/monitor_redis.user.js
 // @downloadURL http://2le.me/static/monitor_redis.user.js
@@ -17,13 +17,13 @@
 // @version     1.0.0
 // ==/UserScript==
 
-var mySearchBtn = '<input type="\"button\"" id="\"mySearchBtn\"" class="\'span3" btn="" grey="" left2\'="" style="\"margin-left:" 20px\"="" onclick="\"\"" value="\"����\"">';
+var mySearchBtn = '<input type="\"button\"" id="\"mySearchBtn\"" class="\'span3" btn="" grey="" left2\'="" style="\"margin-left:" 20px\"="" onclick="\"\"" value="\"闪拍\"">';
 $("#query").after(mySearchBtn);
 $("#query").css({width:'11%',padding:'7px 2px 7px 2px'});
 $("#mySearchBtn").css({width:'11%',padding:'7px 2px 7px 2px'});
 $("#mySearchBtn").click(function(){
-    $("#systemNameField").val('��������');
-    $("#systemNameField_chzn a span").text('��������');
+    $("#systemNameField").val('苏宁闪拍');
+    $("#systemNameField_chzn a span").text('苏宁闪拍');
     $("#query").click();
 });
 
